5c3abdc985b9a66d24cab202861e1341a01fd963
Litalino <99712477+Litalino@users.noreply.github.com>
2023-12-26 09:34:37 +0700

    Update 2022_01_07_000000_add_countrycode_to_db.php

--- migrations/2022_01_07_000000_add_countrycode_to_db.php
+++ migrations/2022_01_07_000000_add_countrycode_to_db.php
@@ -1,6 +1,6 @@
 <?php
 
-//use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 //use Illuminate\Support\Facades\Schema;
 use Flarum\Database\Migration;
@@ -18,7 +18,14 @@ return [
         return Migration::addColumns('users', [
             'countryCode' => ['type' => 'text']
         ]);
+    },
+    'down' => function (Builder $schema) {
+        $schema->table('user', function (Blueprint $table) {
+            return;
+            //$table->dropColumn('countryCode');
+        });
     }
+
 ];
 
 
